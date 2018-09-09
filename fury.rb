87e9aab67516e4399517755c5c196f8434594fbb
Roland Kuhn <rk@rkuhn.info>
2018-09-09 11:51:34 +0200

    fix classpath

--- fury.rb
+++ fury.rb
@@ -4,13 +4,14 @@
 class Fury < Formula
   desc "Fury against the ecosystem"
   homepage "https://fury.build/"
-  version "0.1"
-  url "https://rkuhn.info/fury-0.1.tar.gz"
-  sha256 "513d54fa5341be44b87d9e0c89535134ac5616bc512bcffff7a45b08c321d543"
+  version "0.1.1"
+  url "https://rkuhn.info/fury-0.1.1.tar.gz"
+  sha256 "9acdb0d554d36d8c1874a7a21b3f94935033e4b7f3555cbaa7f39fd82af4f6ac"
   depends_on "nailgun"
   depends_on "coursier/formulas/coursier"
   depends_on "scalacenter/bloop/bloop"
   depends_on :java => "1.8+"
+  depends_on "scala"
 
   def install
     zsh_completion.install "_fury"
